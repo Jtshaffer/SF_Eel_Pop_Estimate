b735d76696a97b2dd5a8f6221e181c40f6ee6950
Jason Shaffer <54858035+Jtshaffer@users.noreply.github.com>
2023-11-30 16:55:23 -0800

    Updates to code and a new function

---
+++ Download/Daily_passage_plot_fxn.R
@@ -0,0 +1,56 @@
+#Daily Passage Plot
+
+daily_passage_plot<- function(Data1= XYZ, Data2= ZYX ){
+  Data2<-Data2 %>% 
+    mutate(Date= as.POSIXct(Date))
+  
+  # function to scale the axis automatically 
+  max_first  <- max(Data2$Daily.Net,na.rm = T) +1   # Specify max of first y axis
+  max_second <- max(Data1$Flow_Inst,na.rm = T) +5 # Specify max of second y axis
+  min_first  <- min(Data2$Daily.Net,na.rm = T) -1  # Specify min of first y axis
+  min_second <- min(Data1$Flow_Inst,na.rm = T)  -15 # Specify min of second y axis
+  
+  
+  # scale and shift variables calculated based on desired mins and maxes
+  scale = (max_second - min_second)/(max_first - min_first)
+  shift = (min_first) - (min_second) 
+  
+  # Function to scale secondary axis
+  scale_function <- function(x, scale, shift){
+    return ((x)*scale - shift)
+  }
+  
+  # Function to scale secondary variable values
+  inv_scale_function <- function(x, scale, shift){
+    return ((x + shift)/scale)
+  }
+  
+  
+  plot <- ggplot() +
+    #geom_point(shape = 20,size = 2) +
+    #stat_summary_bin( fun = "median", colour = "red", size = 2, geom = "point",bins = 4)+
+    geom_line(aes(x =Data1$dateTime, y = inv_scale_function(Data1$Flow_Inst, scale, shift), color ='blue')) +
+    scale_y_continuous(limits = c(min_first, max_first),
+                       sec.axis = sec_axis(~scale_function(., scale, shift),
+                                           name = "USGS Miranda gage flow (cfs)"))+
+    scale_x_datetime(date_breaks = "1 day",date_labels = "%b %d" )+
+    # geom_point(data= Data2, aes(x=Date , y = Daily.Net), shape = 0,size = 2) +
+    geom_point(aes(x=Data2$Date , y = Data2$Daily.Net), shape = 18,size = 1.75) +
+    # geom_text(data = Data2, aes(x = Date, y = Daily.Net, label = Daily.Net),nudge_y = 25, ) +
+    # geom_text_repel(data = Data2, aes(x = Date, y = Daily.Net, label = Daily.Net) ) +
+    ylab("Daily passage estimate (fish > 40 cm)")+
+    xlab("Date")+
+    scale_color_manual(name = "",
+                       breaks = c("blue"),
+                       labels = c("USGS Miranda gage flow (cfs)"),
+                       values = c("blue" = "blue"))+
+    theme(legend.position = "bottom",
+          plot.background = element_rect(inherit.blank = T),
+          panel.background = element_rect(fill = NA),
+          panel.border = element_rect(linewidth = 1,linetype = 1,
+                                      fill = NA),
+          axis.text.x = element_text(angle = 45, vjust = .5))
+  
+  plot
+  
+}
--- Download/Pop_Est.R
+++ Download/Pop_Est.R
@@ -8,51 +8,51 @@ source("Download/Missing_Hours_fxn.R")
 source("Download/Size_correction_fxn.R")
 source("Download/Missing_Day_fxn.R")
 source("Download/Flow_Data_Fxn.R")
+source("Download/Daily_passage_plot_fxn.R")
 
 
 Data<- as.data.frame(readxl::read_xlsx("Data/Example_Data.xlsx"))
 Data$Minute<- ifelse(grepl(".5",Data[["Hour"]],fixed = T) == T,30,0)
 Data$Hour<- as.numeric(gsub(".5","",Data[["Hour"]],fixed = T))
 Data<- Data %>% 
-  # filter(!Minute == 30) %>%  #Currently this script only supports one 10 minute count per hour
+  # filter(!Minute == 30) %>%  
   mutate(Date = paste(Year,Month,Day,Hour,Minute, sep = "-")) %>% 
   mutate(Date = ymd_hm(Date))
 
 
 # Obtain USGS data for site of interest. URL to helpful page: https://waterdata.usgs.gov/blog/dataretrieval/
-flowdata<- Flow_data_fxn(siteNo = "11476500",pCode= "00060",start.date= "2022-10-31",end.date = as.character(today()))
+flowdata<- Flow_data_fxn(siteNo = "11476500",pCode= "00060",start.date= "2023-10-6",end.date = as.character(today()))
 
-#Join the reviewed data and USGS flow data
-Data<- left_join(Data,flowdata, by = c("Date" = "dateTime"))
 
 # Remove the counts that are less than 40 cm to correct the net counts. Optional* 
 # Data<-Size_correction(Data)
 
 
 ## Hourly counts *Includes option to interpolate data to missing intervals ----
-Hourly_Data<- Hourly_counts(Data,interpolate= F,twenty_min_file = T)
+# The current interpolation procedure: for any missing hourly passage estimate, the passage counts 24hrs prior to and after the missing hour are averaged and applied to the missing period. 
+Hourly_Data<- Hourly_counts(Data,interpolate= T,twenty_min_file = T)
 
-#Estimating daily net movement *Includes option to correct for missing intervals
+#Estimating daily net movement *Includes a correction for missing intervals.
+# The current correction procedure: the total number of missing intervals is computed and then divided by the total number of possible intervals (48 for 20min files) to obtain a correction factor. The daily net movement is then multiplied by the correction factor to obtain the number of fish that would have been expected to move during the missing time intervals. These fish are then added to the daily net movement to obtain the total daily movement. 
 Daily_Data<- Daily_counts(Data = Hourly_Data, correction = T, twenty_min_file = F)
 
 
-#Adding in the missing days correction factor and calculating monthly net movement- Not operating properly 11/28/2023
-# days.per.month<- Daily_Data %>% 
-#   group_by(Month) %>% 
-#   summarise(Days= length(Day))
-# 
-# Monthly_passage<- Daily_Data %>% 
-#   group_by(Month) %>% 
-#   summarise(Monthlynet= sum(Corrected.Daily.Net,na.rm = T),
-#             missing_day_vals= n_distinct(which(Corrected.Daily.Net == 0.00))) %>% 
-#   mutate(monthcorrectionfactor = missing_day_vals/days.per.month$Days,
-#          corrected.monthly.net = round(Monthlynet + (Monthlynet*monthcorrectionfactor ))) %>% 
-#   ungroup()
+#Monthly net movement
+Monthly_Data<- Daily_Data %>%
+  group_by(Month) %>%
+  summarise(Monthlynet= sum(Corrected.Daily.Net,na.rm = T))
+
 
 
 #Viewing daily passage 
 Daily_Data
 
 #Viewing monthly passage 
-Monthly_passage
+Monthly_Data
+
+
+# Daily passage estimate figure
+# This function produces a plot showing the daily passage estimates plotted against the USGS flow gauge at Miranda
+daily_passage_plot(flowdata,Daily_Data)
+
 
